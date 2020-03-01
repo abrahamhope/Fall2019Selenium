94f80e4719f612c194c84a2fc9b5d97b87662041
abrahamhope <abrahamisaachope@gmail.com>
2020-03-01 12:53:45 -0600

    MORE CODE ADDED

--- src/test/java/com/automation/tests/day2/BasicNavigation.java
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -5,13 +5,26 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BasicNavigation {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         // to start selenium script we need to:
         // setup webdriver and create webdriver object
         WebDriverManager.chromedriver().setup();
         ChromeDriver driver = new ChromeDriver();
         // in selenium, everything start from WebDriver
+        driver.get("http://google.com"); // to open a website
+        Thread.sleep(3000);
+        String title = driver.getTitle(); // returns <title> Some title</title>
+        String expectedTitle="Google";
 
+        System.out.println("WebPage Title is: "+title);
+        if(expectedTitle.equals(title)){
+            System.out.println("TEST PASSED!");
+        }else{
+            System.out.println("TEST FAILED!");
+        }
+
+
+        driver.close(); // to close the driver
 
     }
 }
