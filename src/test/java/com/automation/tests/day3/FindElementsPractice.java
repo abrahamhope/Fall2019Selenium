1871f4fa3e76bd5aa0866da1010413423a49b915
abrahamhope <abrahamisaachope@gmail.com>
2020-03-03 18:44:14 -0600

    day 3 is added

--- src/test/java/com/automation/tests/day3/FindElementsPractice.java
+++ src/test/java/com/automation/tests/day3/FindElementsPractice.java
@@ -1,12 +1,13 @@
 package com.automation.tests.day3;
 
+import com.automation.utilities.DriverMaker;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FindElementsPractice {
     public static void main(String[] args) {
-        WebDriverManager.chromedriver().setup();
-        ChromeDriver driver = new ChromeDriver();
+        WebDriver driver = DriverMaker.setupWebDriver("chrome");
 
         driver.get("http://practice.cybertekschool.com");
     }
--- src/test/java/com/automation/utilities/DriverMaker.java
+++ src/test/java/com/automation/utilities/DriverMaker.java
@@ -4,6 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.opera.OperaDriver;
 
 
@@ -24,7 +25,10 @@ public class DriverMaker {
         }else if(browserName.equalsIgnoreCase("firefox")){
             WebDriverManager.firefoxdriver().setup();
             return new FirefoxDriver();
-        }else {
+        }else if(browserName.equalsIgnoreCase("ie")) {
+            WebDriverManager.iedriver().setup();
+            return new InternetExplorerDriver();
+        }else{
             WebDriverManager.operadriver().setup();
             return new OperaDriver();
         }
