1957c2ea53dd82f1f6914955a86c3a252dd5250d
abrahamhope <abrahamisaachope@gmail.com>
2020-03-08 14:29:22 -0500

    day 4 warm up

--- src/test/java/com/automation/tests/day6/NoSelectDropdown.java
+++ src/test/java/com/automation/tests/day6/NoSelectDropdown.java
@@ -1,7 +1,24 @@
 package com.automation.tests.day6;
 
+import com.automation.utilities.BrowserUtils;
+import com.automation.utilities.DriverFactory;
+import com.google.gson.internal.$Gson$Preconditions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class NoSelectDropdown {
     public static void main(String[] args) {
+        WebDriver driver = DriverFactory.createWebDriver("chrome");
+        driver.get("http://practice.cybertekschool.com/dropdown");
+        BrowserUtils.wait(3);
 
+        driver.findElement(By.id("dropdownMenuLink")).click(); //to expand dropdown
+        BrowserUtils.wait(2);
+
+        driver.findElement(By.linkText("Google")).click();
+
+
+        BrowserUtils.wait(3);
+        driver.quit();
     }
 }
---
+++ src/test/java/com/automation/tests/day6/SelectByValue.java
@@ -0,0 +1,30 @@
+package com.automation.tests.day6;
+
+import com.automation.utilities.BrowserUtils;
+import com.automation.utilities.DriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class SelectByValue {
+    public static void main(String[] args) {
+        WebDriver driver = DriverFactory.createWebDriver("chrome");
+        driver.get("http://practice.cybertekschool.com/dropdown");
+        BrowserUtils.wait(3);
+
+        Select stateSelected = new Select(driver.findElement(By.id("state")));
+        stateSelected.selectByValue("DC");
+
+        String expected = "District Of Columbia";
+        String actual = stateSelected.getFirstSelectedOption().getText();
+
+        if(expected.equals(actual)){
+            System.out.println( "TEST PASSED");
+        }else{
+            System.out.println("TEST FAILED");
+        }
+
+        BrowserUtils.wait(2);
+        driver.quit();
+    }
+}
