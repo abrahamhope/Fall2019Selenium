1865158af249574689f015bd56b98a0033a032be
abrahamhope <abrahamisaachope@gmail.com>
2020-03-14 12:56:16 -0500

    day 8

--- src/test/java/com/automation/tests/day8/SearchTestss.java
+++ src/test/java/com/automation/tests/day8/SearchTestss.java
@@ -1,13 +1,40 @@
 package com.automation.tests.day8;
 
+import com.automation.utilities.BrowserUtils;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class SearchTestss {
     private WebDriver driver;
+
+    @Test
+    public void googleSearchTest(){
+        driver.get("http://google.com");
+        driver.findElement(By.name("q")).sendKeys("java", Keys.ENTER);
+
+        BrowserUtils.wait(2);
+
+        List<WebElement> searchItems = driver.findElements(By.tagName("h3"));
+        for (WebElement searchItem:searchItems){
+            String var= searchItem.getText();
+            if(!var.isEmpty()){
+                System.out.println(var);
+                Assert.assertTrue(var.toLowerCase().contains("java"));
+            }
+        }
+
+    }
+
 
     @BeforeMethod
     public void setup(){
