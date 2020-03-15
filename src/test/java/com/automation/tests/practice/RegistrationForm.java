96d58b19bb1db81ce28a0cb18af3d5e3fa33900d
abrahamhope <abrahamisaachope@gmail.com>
2020-03-15 15:26:46 -0500

    VY Track

---
+++ src/test/java/com/automation/tests/practice/RegistrationForm.java
@@ -0,0 +1,37 @@
+package com.automation.tests.practice;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class RegistrationForm {
+    private WebDriver driver;
+    private String URL= "http://practice.cybertekschool.com/registration_form";
+
+
+
+    @BeforeMethod
+    public void setup(){
+        WebDriverManager.chromedriver().version("79").setup();
+        driver = new ChromeDriver();
+        driver.get(URL);
+        driver.manage().window().maximize();
+
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        // if WebDriver object alive,
+        if(driver!=null){
+            // close browser, close session
+            driver.quit();
+            // destroy driver object for sure.
+            driver=null;
+        }
+    }
+
+
+
+}
--- src/test/java/com/automation/tests/vytrack/login/LoginPageTests.java
+++ src/test/java/com/automation/tests/vytrack/login/LoginPageTests.java
@@ -1,19 +1,61 @@
 package com.automation.tests.vytrack.login;
 
+import com.automation.utilities.BrowserUtils;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.security.Key;
+
 import static org.testng.Assert.*;
 
 public class LoginPageTests {
     private WebDriver driver;
+    private String URL = "https://qa2.vytrack.com/user/login";
+    //credentials for store manager
+    private String username = "storemanager51";
+    private String password = "UserUser123";
+    private By usernameBy = By.id("prependedInput");
+    private By passwordBy =By.id("prependedInput2");
+    private By loginBtnBy = By.id("_submit");
+    private By warningMessageBy= By.xpath("//*[@id=\"login-form\"]/fieldset/div[1]");
+
+    @Test(description = "verify that warning message displays when user enters invalid username")
+    public void invalidUsername(){
+        driver.get(URL);
+        driver.findElement(usernameBy).sendKeys("invalidusername");
+        driver.findElement(passwordBy).sendKeys("UserUser123");
+        driver.findElement(loginBtnBy).click();
+        BrowserUtils.wait(3);
+
+        WebElement warningElement = driver.findElement(warningMessageBy);
+        assertTrue(warningElement.isDisplayed());
+        BrowserUtils.wait(2);
+    }
+
+    @Test(description = "login as store manager and verify that title equals to Dashboard")
+    public void loginAsStoreManager(){
+        driver.get(URL);
+        driver.findElement(usernameBy).sendKeys(username);
+        driver.findElement(passwordBy).sendKeys(password);
+        driver.findElement(loginBtnBy).click();
+        BrowserUtils.wait(3);
+        assertEquals(driver.getTitle(),"Dashboard");
+        BrowserUtils.wait(3);
+    }
+
 
     @BeforeMethod
     public void setup(){
         WebDriverManager.chromedriver().version("79").setup();
         driver = new ChromeDriver();
+        driver.get(URL);
         driver.manage().window().maximize();
 
     }
