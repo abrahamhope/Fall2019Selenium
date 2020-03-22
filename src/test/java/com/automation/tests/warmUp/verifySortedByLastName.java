73a3068ce71886c55b799d7ad5f1510a9df65126
abrahamhope <abrahamisaachope@gmail.com>
2020-03-22 15:09:50 -0500

    day 11

--- src/test/java/com/automation/tests/warmUp/verifySortedByLastName.java
+++ src/test/java/com/automation/tests/warmUp/verifySortedByLastName.java
@@ -37,8 +37,33 @@ public void verifySortedLastName(){
 //    Collections.sort(lastNamesExpected);
 //
 //    Assert.assertEquals(lastNamesActual,lastNamesExpected);
+/**
+ * Go to http://practice.cybertekschool.com/tables
+ * Click on "Last name" column name
+ * Verify that table is sorted by last name in alphabetic order.
+ *
+ * The Java String compareTo() method is used for comparing two strings lexicographically.
+ * Each character of both the strings is converted into a Unicode value for comparison.
+ * If both the strings are equal then this method returns 0 else it returns positive or negative value.
+ * The result is positive if the first string is lexicographically greater than
+ * the second string else the result would be negative.
+ *
+ *  This method is coming from Comparable interface.
+ *  If you want ot be able to sort collection of some class
+ *  you need to implement this interface
+ *  and override compareTo method
+ *
+ "a".compareTo("b") = -1
+ 61 - 62 = -1
+ a is before b
+ "a".compareTo("a")
+ 61 - 61 = 0
+ 0 means 2 strings are equals
+ *
+ */
 
-        List<WebElement> column= driver.findElements(By.xpath("//table[1]/tbody/tr/td[1]"));
+
+    List<WebElement> column= driver.findElements(By.xpath("//table[1]/tbody/tr/td[1]"));
 
     for (int i = 0; i < column.size() - 1; i++) {
         String value= column.get(i).getText();
