eb1749971c13babbe4d41d7d206c3f8106036507
abrahamhope <abrahamisaachope@gmail.com>
2020-03-24 18:39:22 -0500

    day 13

---
+++ src/test/java/com/automation/tests/day13/SystemProperties.java
@@ -0,0 +1,13 @@
+package com.automation.tests.day13;
+
+public class SystemProperties {
+
+    public static void main(String[] args) {
+
+        System.out.println(System.getProperty("user.dir"));
+        System.out.println(System.getProperty("os.name"));
+        System.out.println(System.getProperty("os.arch"));
+
+
+    }
+}
