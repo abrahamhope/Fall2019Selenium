319bf7fe9e42bc4a4a77be037ecd61c11f58be51
abrahamhope <abrahamisaachope@gmail.com>
2020-03-28 10:32:42 -0500

    office hour

--- src/test/java/com/automation/tests/officeHour/iframesPractice.java
+++ src/test/java/com/automation/tests/officeHour/iframesPractice.java
@@ -1,5 +1,6 @@
 package com.automation.tests.officeHour;
 
+import com.automation.utilities.BrowserUtils;
 import com.automation.utilities.DriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -27,15 +28,30 @@ public class iframesPractice {
     }
 
     @Test
-    public void Test(){
+    public void Test() {
         driver.get("http://practice.cybertekschool.com/nested_frames");
         driver.manage().window().maximize();
         driver.switchTo().frame("frame-top");
 
         List<WebElement> frameList = driver.findElements(By.xpath("//frame"));
+        /*
+            we have stored all frames webelements inside the list
+            1. Loop through list
+            2. swithch to each frame
+                a. get test from body
+                b. switch to parent
+         */
+        for (WebElement each : frameList) {
+
+            BrowserUtils.wait(2);
+            driver.switchTo().frame(each);
+            String bodyText = driver.findElement(By.tagName("body")).getText();
+            System.out.println("bodyText = " + bodyText);
+            driver.switchTo().parentFrame();
+
+        }
+
 
     }
-
-
 
 }
